1cfe0dd350a14d25c5fa06bd9ae99ea4fadb903d
Your Name <you@example.com>
2021-09-24 22:57:38 -0500

    Lógica para añadir alimentos a la orden de la mesa añadida

--- main.py
+++ main.py
@@ -20,3 +20,13 @@ def assign_table(table_number, name, vip_status=False):
   tables[table_number]['name'] = name
   tables[table_number]['vip_status'] = vip_status
   tables[table_number]['order'] = {}
+
+def assign_food_items(table_number, **order_items):
+  food = order_items.get('food')
+  drinks = order_items.get('drinks')
+  tables[table_number]['order']['food_items'] = food
+  tables[table_number]['order']['drinks'] = drinks
+
+assign_table(2, 'Daniel', True)
+assign_food_items(2, drinks='Diet Coke', food_items='Burger with Fries')
+print(tables)
