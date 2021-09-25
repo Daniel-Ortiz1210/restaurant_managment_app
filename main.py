859a60e89bce1f5976da9e2702528383e1e0a718
Your Name <you@example.com>
2021-09-24 22:09:00 -0500

    Lista de restaurantes añadida

---
+++ main.py
@@ -0,0 +1,17 @@
+tables = {
+  1: {
+    'name': 'Jiho',
+    'vip_status': False,
+    'order': {
+      'drinks': 'Orange Juice, Apple Juice',
+      'food_items': 'Pancakes',
+      'total': [534.50, 20.0, 5]
+    }
+  },
+  2: {},
+  3: {},
+  4: {},
+  5: {},
+  6: {},
+  7: {},
+}
