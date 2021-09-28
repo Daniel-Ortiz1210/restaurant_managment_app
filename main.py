55353b47d940627b34e4114c1f945bbbebe962f6
Your Name <you@example.com>
2021-09-27 23:25:07 -0500

    Recibir datos del usuario para asignar mesa

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 tables = {
   1: {
-    'name': 'Jiho',
+    'name': ['Jiho'],
     'vip_status': False,
     'order': {
       'drinks': 'Orange Juice, Apple Juice',
@@ -16,10 +16,15 @@ tables = {
   7: {},
 }
 
-def assign_table(table_number, name, vip_status=False): 
+def assign_table(table_number, name, vip_status=False):
+  if vip_status == 'SI':
+    vip_status = True
+  # ¿Qué pasa cuando una mesa ya está ocupada?
+  # ¿Qué pasa cuando todas las mesas ya están ocupadas?
   tables[table_number]['name'] = name
   tables[table_number]['vip_status'] = vip_status
   tables[table_number]['order'] = {}
+  print('¡Mesa asignada correctamente!')
 
 def assign_food_items(table_number, **order_items):
   food = order_items.get('food')
@@ -35,4 +40,44 @@ def calculate_price_per_person(table_number, total, tip, split):
     for n in tables[table_number]['order']['total']:
         total_order = n
         break
-    print('The total count is', total_order)    
+    print('The total count is', total_order)
+  
+def main():
+  print(
+    '''
+    1. Para asignar una mesa
+    2. Para tomar una orden
+    3. Para calcular la cuenta de una mesa
+    4. Para consultar el status de una mesa
+    5. Para consultar el status de todas las mesas
+    6. Para salir del programa
+    '''
+  )
+  user = input('Qué deseas hacer?: ')
+  while user == '1' or user == '2' or user == '3' or user == '4' or user == '5' or user == '6':
+    if user == '1':
+      table_number = int(input('Escriba el número de mesa: '))
+      names = input('Escriba el nombre de los comensales con apellidos (separados por ", "): ').title().strip().split(', ')
+      vip_status = input('¿Presentan acceso VIP? (S/N): ').upper()
+      assign_table(table_number, names, vip_status)
+      break
+    elif user == '2':
+      print('Esta es opción 2')
+      break
+    elif user == '3':
+      print('Esta es opción 3')
+      break
+    elif user == '4':
+      print('Esta es opción 4')
+      break
+    elif user == '5':
+      print('Esta es opción 5')
+      break
+    elif user == '6':
+      print('Cerrando sesión...')
+      print('Completado. Nos vemos pronto.')
+      break
+
+
+if __name__ == '__main__':
+  main()
